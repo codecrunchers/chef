24cdd19cc5f87b6c6c6508811070f2514c580e9d
alan <alan.ryan@gmail.com>
2016-07-26 23:29:41 +0100

    cannot keep container running

--- cookbooks/apm/recipes/run.rb
+++ cookbooks/apm/recipes/run.rb
@@ -1,5 +1,6 @@
 #
 # Cookbook Name:: apm::run
+#
 # Recipe:: Version
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
@@ -11,6 +12,7 @@ bash "run_tomcat" do
   code <<-EOH
   export JAVA_HOME=/apps/jdk1.8.0_101/
   apache-tomcat-8.0.36/bin/./catalina.sh start
+  while true; do sleep 1; done
   EOH
 end
 
