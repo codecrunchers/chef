58c08cffa718e9ba314dae1d1be73bfbddbf21a8
alan <alan.ryan@gmail.com>
2016-07-26 20:08:05 +0100

    reshuffle

---
+++ repo/cookbooks/apm/attributes/default.rb
@@ -0,0 +1,4 @@
+default['apm']['dest_war'] = '/usr/local/tomcat/webapps/ROOT.war'
+default['apm']['src_war'] = 'http://10.114.10.200:8081/nexus/content/repositories/chef_test/built_artifacts/ROOT.war'
+default['apm']['war_name'] = 'ROOT'
+
