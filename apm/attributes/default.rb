81687d8c45197f9055661ff9b88dd174f924f235
alan <alan.ryan@gmail.com>
2016-07-26 20:06:33 +0100

    move files

--- apm/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['apm']['dest_war'] = '/usr/local/tomcat/webapps/ROOT.war'
-default['apm']['src_war'] = 'http://10.114.10.200:8081/nexus/content/repositories/chef_test/built_artifacts/ROOT.war'
-default['apm']['war_name'] = 'ROOT'
-
